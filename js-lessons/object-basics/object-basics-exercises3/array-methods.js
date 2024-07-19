a02eac4c5d6acde880891669b0b17a5884679b22
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-07-19 17:43:30 +0700

    feat: Implement camelize function that converts kebab case string to camel case string

--- js-lessons/object-basics/object-basics-exercises3/array-methods.js
+++ js-lessons/object-basics/object-basics-exercises3/array-methods.js
@@ -1,1 +1,16 @@
-
+// Convert kebab-case to camelCase
+const camelize = (str) => {
+  // Convert string to lowercase then to array
+  const convertedStr = str.toLowerCase().split('-');
+  let capitalizedArr = [];
+  console.log(convertedStr);
+  // Capitalize items start from second index
+  for (let i = 0; i < convertedStr.length; i +=1) {
+    if (i > 0) {
+      capitalizedArr.push(convertedStr[i][0].toUpperCase() + convertedStr[i].slice(1));
+    } else {
+      capitalizedArr.push(convertedStr[i]);
+    }
+  }
+  return capitalizedArr.join('');
+};
